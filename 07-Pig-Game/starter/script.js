084c5ff6d08ffae75598c8de4515d456bd7ebfad
Alif <alaminalifaa7@gmail.com>
2021-01-27 15:39:10 +0600

    pig game starting condition implemented

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//seecitnf the elements in two ways
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+//starting the game condition
+score0El.textContent = '0';
+score1El.textContent = '0';
+diceEl.classList.add('hidden');
+
+//rolling the dice
+
+const roll =Math.trunc(Math.random() * 6 + 1);
+console.log(roll);
