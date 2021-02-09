3b554c6aa6c5efe342a0564407f35be724894e5f
Alif <alaminalifaa7@gmail.com>
2021-02-09 12:15:36 +0600

    updated the mapty application getting data from the form

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+
+const createBooking = function(flightNum, numPassengers, price){
+
+    const booking ={
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    booking.push(booking);
+}
