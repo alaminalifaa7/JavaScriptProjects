96d01808283219e1970a3bbf8dcc50674cb8c492
Alif <alaminalifaa7@gmail.com>
2021-02-09 12:09:00 +0600

    updated mapty application to render workout getting data from the form

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,10 +11,50 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout{
+  date = new Date();
+  id = (Date.now+'').slice(-10);
+  constructor(coords, distance, duration ){
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout{
+  type = 'running';
+  constructor(coords, distance, duration, cadence){
+    super(coords,distance, duration);
+    this.cadence = cadence; 
+    this.calcPace();
+  }
+  calcPace(){
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+
+}
+
+class Cycling extends Workout{
+  type ='cycling';
+  constructor(coords, distance, duration, elevationGain){
+    super(coords,distance, duration);
+    this.elevationGain= elevationGain; 
+    this.calcSpeed();
+  }
+
+  calcSpeed(){
+    this.speed = this.distance/(this.duration/60);
+    return this.speed;
+  }
+}
+
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
   constructor() {
+    
     this._getPosition();
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
@@ -52,14 +92,59 @@ class App {
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   }
   _newWorkout(e) {
+
+    const validInputs = (...inputs) => 
+    inputs.every(inp => Number.isFinite(inp));
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
     e.preventDefault();
+    //get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
+
+    
+
+    //if workout running, create running object
+    if(type ==='running'){
+      const cadence = +inputCadence.value;
+      //check if data is valid
+      if(!validInputs(distance, duration, cadence) || 
+         !allPositive(distance,duration,cadence)) 
+       return alert('input has to be positive number');
+
+      workout = new Running([lat,lng],distance,duration,cadence);
+    }
+    //if workout cycling, create cycling object
+    if(type ==='cycling'){
+      const elevation  = +inputElevation.value;
+
+      if(!validInputs(distance, duration, elevation) || 
+         !allPositive(distance,duration)) 
+       return alert('input has to be positive number');
+       workout = new Cycling([lat,lng],distance,duration,elevation);
+    }
+
+    //add new object to workout array
+    this.#workouts.push(workout);
+
+// render workout on th emap
+this.renderWorkoutMarker(workout)
+    
+    //render workout on the list
+
+    //hide from + clear input fields
+
+
     //clear input fields
     inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
       '';
-    //Display Marker
-    const { lat, lng } = this.#mapEvent.latlng;
-
-    L.marker([lat, lng])
+    
+  }
+  renderWorkoutMarker(workout){
+    L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -67,10 +152,10 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent('Workout')
+      .setPopupContent('workout')
       .openPopup();
   }
 }
